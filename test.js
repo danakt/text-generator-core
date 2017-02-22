302b8c0d33ec0f807e8233f01349c1984da5204b
Danakt Frost <mail@danakt.ru>
2017-02-22 21:17:22 +0300

    Обновление тестового скрипта

--- test.js
+++ test.js
@@ -4,4 +4,14 @@ const getTemplate  = require('./bin/getTemplate')
 const templates    = require('./templates')
 const flections    = require('./patterns')
 
-console.log(makeSentence(getTemplate(templates), flections))
+function getRandomtext(sentenceNum) {
+    let paragraph = ''
+
+    for (var i = 0; i < sentenceNum; i++) {
+        paragraph += makeSentence(getTemplate(templates()), flections) + ' '
+    }
+
+    return paragraph.trim()
+}
+
+console.log(getRandomtext(10))
