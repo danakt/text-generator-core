cababaf96727265903c9987e4e9d71c1d0947997
Danakt Frost <mail@danakt.ru>
2017-10-11 01:32:32 +0300

    Функции для работы с библиотеками

--- src/dictionary.ts
+++ src/dictionary.ts
@@ -1,44 +1,46 @@
-/** Пункт словаря */
-type DictionaryItem = [string, { [prop: string]: any }]
-
-/** Словарь */
-type Dictionary = DictionaryItem[]
-
-/**
- * Список добавленных словарей
- */
-const dictionaries: { [name: string]: Dictionary } = {}
-
 /**
  * Добавляет словарь в список
- * @param {string}                   name       Название нового словаря
- * @param {Dictionary|Array<string>} dictionary Словарь или массив со словами
+ * @param {string}              name       Название нового словаря
+ * @param {Dictionary|string[]} dictionary Словарь или массив со словами
+ * @param {DictionariesStore}   store      Предыдущее хранилище
  */
-export function addDictionary(name: string, dictionary: Dictionary | string[]) {
-    let newDictionary: Dictionary = []
+export function addDictionary(
+    name:       string,
+    dictionary: (DictionaryItem | string)[],
+    store:      DictionariesStore,
+): DictionariesStore {
+    const newDictionary: Dictionary = dictionary.map(item => {
+        return typeof item === 'string'
+            ? [item, {}] as DictionaryItem
+            : item
+    })
 
-    for (let item of dictionary) {
-        if (Array.isArray(item)) {
-            newDictionary.push(item)
-        } else {
-            newDictionary.push([item, {}])
-        }
+    // Создание нового состояния хранилища
+    const newStore: DictionariesStore = {
+        ...store,
+        [name]: newDictionary,
     }
 
-    dictionaries[name] = newDictionary
+    return newStore
 }
 
 /**
  * Возвращает случайное слово из указанного словаря
+ * @param  {string}            name Название словаря, из которого будет
+ *   осуществляться поиск случайного слова
+ * @param  {DictionariesStore} store Хранилище словарей
  * @return {DictionaryItem}
  */
-export function getRandomItem(name: string): DictionaryItem {
-    if (!dictionaries.hasOwnProperty(name)) {
+export function getRandomItem(
+    name:  string,
+    store: DictionariesStore,
+): DictionaryItem {
+    if (!store.hasOwnProperty(name)) {
         throw new Error('Отсуствует запрашиваемый словарь')
     }
 
-    const randomIndex: number = dictionaries[name].length * Math.random() | 0
-    const randomItem: DictionaryItem = dictionaries[name][randomIndex]
+    const randomIndex: number = store[name].length * Math.random() | 0
+    const randomItem: DictionaryItem = store[name][randomIndex]
 
     return randomItem
 }
