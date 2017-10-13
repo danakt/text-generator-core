25cb02c569c83560571c5a0e5f977283e88553d6
Danakt Frost <mail@danakt.ru>
2017-10-13 18:36:45 +0300

    Отделение функций для создания элементов разных типов

--- src/elements.ts
+++ src/elements.ts
@@ -7,7 +7,7 @@ type ElemenType = 'sentence' | 'fragment' | 'template'
 export interface Element {
     type:       ElemenType
     props:      { [prop: string]: any }
-    children:   (string | Element | DictionaryItem)[]
+    children:   (string | Element | Function)[]
 }
 
 /** Элемент предложения */
@@ -25,71 +25,101 @@ export interface FragmentElement extends Element {
 /** Элемент шаблона */
 export interface TemplateElement extends Element {
     type:       'template'
-    children:   (string | DictionaryItem)[]
+    children:   (string | Function)[]
 }
 
 /**
- * Создаёт элемент шаблона
+ * Создаёт элемент шаблона по типу
  * @param {string}           type       Тип элемента
  * @param {Object}           props      Параметры элемента
  * @param {...object|string} children   Дочерние элементы
  */
 export function createElement(type: 'sentence', props: null | {}, ...children: FragmentElement[]): SentenceElement
 export function createElement(type: 'fragment', props: null | {}, ...children: TemplateElement[]): FragmentElement
-export function createElement(type: 'template', props: null | {}, ...children: (string | DictionaryItem)[]): TemplateElement
-export function createElement(type: ElemenType, props: null | {}, ...children: (string | Element | DictionaryItem)[] ): Element {
+export function createElement(type: 'template', props: null | {}, ...children: (string | Function)[]): TemplateElement
+export function createElement(type: ElemenType, props: null | {}, ...children: (string | Element | Function)[] ): Element {
     const propsFallBack = props == null ? {} : props
 
     // Проврека иерархии типов
     // sentence —> fragment —> template
     switch (type) {
         case 'sentence': {
-            children.forEach((child: Element) => {
-                if (child.type !== 'fragment') {
-                    throw new Error('Элемент «sentence» может иметь только дочерние элементы с типом «fragment»')
-                }
-            })
-
-            return {
-                type,
-                props: propsFallBack,
-                children: children as FragmentElement[],
-            }
+            return createSentenceElement(propsFallBack, ...children as FragmentElement[])
         }
 
         case 'fragment': {
-            children.forEach((child: Element) => {
-                if (child.type !== 'template') {
-                    throw new Error('Элемент «template» может иметь только дочерние элементы с типом «template»')
-                }
-            })
-
-            return {
-                type,
-                props: propsFallBack,
-                children: children as TemplateElement[],
-            }
+            return createFragmentElement(propsFallBack, ...children as TemplateElement[])
         }
 
         case 'template': {
-            children.forEach((child: Element) => {
-                if (typeof child !== 'string' && typeof child !== 'function') {
-                    throw new Error(
-                        'Элемент «template» может иметь в качестве дочерних элементов только строки или функции, ' +
-                        'возвращаемые элементы словаря.'
-                    )
-                }
-            })
-
-            return {
-                type,
-                props: propsFallBack,
-                children
-            }
+            return createTemplateElement(propsFallBack, ...children as (string | Function)[])
         }
 
         default: {
             throw new Error('Неизвестный тип элемента')
         }
+    }
+}
+
+/**
+ * Создаёт элемент предложения
+ * @param  {object}              props     Параметры элемента
+ * @param  {...FragmentElement}  children  Дочерние элементы
+ * @return {SentenceElement}
+ */
+export function createSentenceElement(props: object, ...children: FragmentElement[]): SentenceElement {
+    children.forEach((child: Element) => {
+        if (child.type !== 'fragment') {
+            throw new Error('Элемент «sentence» может иметь только дочерние элементы с типом «fragment»')
+        }
+    })
+
+    return {
+        type: 'sentence',
+        props,
+        children,
+    }
+}
+
+/**
+ * Создаёт элемент фрагмента предложения
+ * @param  {object}              props     Параметры элемента
+ * @param  {...TemplateElement}  children  Дочерние элементы
+ * @return {FragmentElement}
+ */
+export function createFragmentElement(props: object, ...children: TemplateElement[]): FragmentElement {
+    children.forEach((child: Element) => {
+        if (child.type !== 'template') {
+            throw new Error('Элемент «fragment» может иметь только дочерние элементы с типом «template»')
+        }
+    })
+
+    return {
+        type: 'fragment',
+        props,
+        children,
+    }
+}
+
+/**
+ * Создаёт элемент шаблона фрагмента
+ * @param  {object}               props     Параметры элемента
+ * @param  {...(string|Function)} children  Дочерние элементы
+ * @return {TemplateElement}
+ */
+export function createTemplateElement(props: object, ...children: (string | Function)[]): TemplateElement {
+    children.forEach((child: string | Function) => {
+        if (typeof child !== 'string' && typeof child !== 'function') {
+            throw new Error(
+                'Элемент «template» может иметь в качестве дочерних элементов только строки или функции, ' +
+                'возвращаемые элементы словаря.'
+            )
+        }
+    })
+
+    return {
+        type: 'template',
+        props,
+        children
     }
 }
