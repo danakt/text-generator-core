22373790430c011760adfd3f6ce89a06da04ef31
Danakt Frost <mail@danakt.ru>
2017-10-22 19:26:01 +0300

    Обнолвение тестирования создания элементов

--- test/elements.tsx
+++ test/elements.tsx
@@ -6,81 +6,71 @@ import { createElement } from '../src/elements'
  * @function createElement
  */
 describe('Создание элемента шаблона', () => {
-    it('Проврека структуры получившегося объекта', () => {
-        const someFunction = () => {}
-        const sentenceTempalte = (
-            createElement('sentence', { someprop: 'somevalue' },
-                createElement('fragment', null,
-                    createElement('template', {}, 'Lorem ipsum'),
-                    createElement('template', null, 'Dolor sit amet ', someFunction),
-                )
-            )
+  it('Проврека структуры получившегося объекта', () => {
+    const store = {
+      'словарь': [
+        ['Элемент словаря', {}]
+      ]
+    }
+
+    const sentenceTempalte = (
+      createElement('sentence', { someprop: 'somevalue' },
+        createElement('fragment', null,
+          ['Элемент словаря', { someprop: 'somevalue' }],
+          'Обычный текст',
+          createElement('word', { type: 'словарь', store: store })
         )
+      )
+    )
 
-        expect(sentenceTempalte).to.deep.eq({
-            type:  'sentence',
-            props: { someprop: 'somevalue' },
-            children: [
-                {
-                    type:'fragment',
-                    props: {},
-                    children: [
-                        {
-                            type: 'template',
-                            props: {},
-                            children: [
-                                'Lorem ipsum'
-                            ]
-                        }, {
-                            type: 'template',
-                            props: {},
-                            children: [
-                                'Dolor sit amet ',
-                                someFunction
-                            ]
-                        }
-                    ]
-                }
-            ]
-        })
+    expect(sentenceTempalte).to.deep.eq({
+      type: 'sentence',
+      props: { someprop: 'somevalue' },
+      children: [{
+        type: 'fragment',
+        props: {},
+        children: [
+          ['Элемент словаря', {
+            'someprop': 'somevalue'
+          }],
+          'Обычный текст',
+          ['Элемент словаря', {}]
+        ]
+      }]
     })
+  })
 
-    it('Проврека создания шаблона при помощи JSX', () => {
-        const someFunction = () => {}
-        const sentenceTempalte = (
-            <sentence someprop="somevalue">
-                <fragment>
-                    <template>Lorem ipsum</template>
-                    <template>Dolor sit amet {someFunction}</template>
-                </fragment>
-            </sentence>
-        )
+  it('Проврека создания шаблона при помощи JSX', () => {
+    const store = {
+      'словарь': [
+        ['Элемент словаря', {}]
+      ]
+    }
 
-        expect(sentenceTempalte).to.deep.eq({
-            type:  'sentence',
-            props: { someprop: 'somevalue' },
-            children: [
-                {
-                    type:'fragment',
-                    props: {},
-                    children: [
-                        {
-                            type: 'template',
-                            props: {},
-                            children: [
-                                'Lorem ipsum'
-                            ]
-                        }, {
-                            type: 'template',
-                            props: {},
-                            children: [
-                                'Dolor sit amet ',
-                                someFunction
-                            ]
-                        }
-                    ]
-                }
-            ]
-        })
+    const sentenceTempalte = (
+      <sentence someprop="somevalue">
+        <fragment>
+          {['Элемент словаря', { someprop: 'somevalue' }]}
+          Обычный текст
+          <word type="словарь" store={store}/>
+        </fragment>
+      </sentence>
+    )
+
+    expect(sentenceTempalte).to.deep.eq({
+      type: 'sentence',
+      props: { someprop: 'somevalue' },
+      children: [{
+        type: 'fragment',
+        props: {},
+        children: [
+          ['Элемент словаря', {
+            'someprop': 'somevalue'
+          }],
+          'Обычный текст',
+          ['Элемент словаря', {}]
+        ]
+      }]
     })
+  })
 })
