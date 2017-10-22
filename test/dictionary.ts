ad1611838bec5625854c10a41d21caf21e9f639c
Danakt Frost <mail@danakt.ru>
2017-10-22 19:19:13 +0300

    Индентация

--- test/dictionary.ts
+++ test/dictionary.ts
@@ -6,31 +6,31 @@ import { addDictionary, getRandomItem, createStore } from '../src/dictionary'
  * @function addDictionary
  */
 describe('Добавление словарей', () => {
-    it('Добавление словаря из строкового массива', () => {
-        const store = addDictionary([
-            'раз',
-            'два',
-            'три'
-        ], 'словарь', {})
+  it('Добавление словаря из строкового массива', () => {
+    const store = addDictionary([
+      'раз',
+      'два',
+      'три'
+    ], 'словарь', {})
 
-        expect(store['словарь']).not.undefined
-        expect(store['словарь'][0]).to.deep.eq(['раз', {}])
-        expect(store['словарь'][1]).to.deep.eq(['два', {}])
-        expect(store['словарь'][2]).to.deep.eq(['три', {}])
-    })
+    expect(store['словарь']).not.undefined
+    expect(store['словарь'][0]).to.deep.eq(['раз', {}])
+    expect(store['словарь'][1]).to.deep.eq(['два', {}])
+    expect(store['словарь'][2]).to.deep.eq(['три', {}])
+  })
 
-    it('Добавление словаря из списка слов с параметрами', () => {
-        const store = addDictionary([
-            ['четыре', { 'р': 'м' }],
-            ['пять', { 'р': 'м' }],
-            ['шесть', { 'р': 'м' }],
-        ], 'словарь', {})
+  it('Добавление словаря из списка слов с параметрами', () => {
+    const store = addDictionary([
+      ['четыре', { 'р': 'м' }],
+      ['пять', { 'р': 'м' }],
+      ['шесть', { 'р': 'м' }],
+    ], 'словарь', {})
 
-        expect(store['словарь']).not.undefined
-        expect(store['словарь'][0]).to.deep.eq(['четыре', { 'р': 'м' }])
-        expect(store['словарь'][1]).to.deep.eq(['пять', { 'р': 'м' }])
-        expect(store['словарь'][2]).to.deep.eq(['шесть', { 'р': 'м' }])
-    })
+    expect(store['словарь']).not.undefined
+    expect(store['словарь'][0]).to.deep.eq(['четыре', { 'р': 'м' }])
+    expect(store['словарь'][1]).to.deep.eq(['пять', { 'р': 'м' }])
+    expect(store['словарь'][2]).to.deep.eq(['шесть', { 'р': 'м' }])
+  })
 })
 
 /**
@@ -38,25 +38,25 @@ describe('Добавление словарей', () => {
  * @function createStore
  */
 describe('Создание хранилища словарей', () => {
-    it('Создание хранилища из карты словарей', () => {
-        const store = createStore({
-            'первый словарь': ['раз', 'два', 'три'],
-            'второй словарь': [['раз', { prop: 'value' }], ['два', {}], 'три']
-        })
-
-        expect(store).to.deep.eq({
-            'первый словарь': [
-                ['раз', {}],
-                ['два', {}],
-                ['три', {}],
-            ],
-            'второй словарь': [
-                ['раз', { 'prop': 'value' }],
-                ['два', {}],
-                ['три', {}],
-            ],
-        })
+  it('Создание хранилища из карты словарей', () => {
+    const store = createStore({
+      'первый словарь': ['раз', 'два', 'три'],
+      'второй словарь': [['раз', { prop: 'value' }], ['два', {}], 'три']
     })
+
+    expect(store).to.deep.eq({
+      'первый словарь': [
+        ['раз', {}],
+        ['два', {}],
+        ['три', {}],
+      ],
+      'второй словарь': [
+        ['раз', { 'prop': 'value' }],
+        ['два', {}],
+        ['три', {}],
+      ],
+    })
+  })
 })
 
 /**
@@ -64,12 +64,12 @@ describe('Создание хранилища словарей', () => {
  * @function getRandomItem
  */
 describe('Получение случайного слова из словаря', () => {
-    it('Наличие полученного слова в словаре', () => {
-        const initDictArray = ['раз', 'два', 'три']
+  it('Наличие полученного слова в словаре', () => {
+    const initDictArray = ['раз', 'два', 'три']
 
-        const store = addDictionary(initDictArray, 'словарь', {})
-        const randomItem = getRandomItem('словарь', {}, store)
+    const store = addDictionary(initDictArray, 'словарь', {})
+    const randomItem = getRandomItem('словарь', {}, store)
 
-        expect(initDictArray).to.include(randomItem[0])
-    })
+    expect(initDictArray).to.include(randomItem[0])
+  })
 })
